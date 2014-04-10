9396977327b09046b4f9fee48397c1011b92d5ff
kiran <>
2014-04-10 14:21:02 +0530

    packaging ffplay inside the jar file. copying ffplay binary into temp folder once program starts.

--- src/Globals/GlobalData.java
+++ src/Globals/GlobalData.java
@@ -7,6 +7,12 @@
 package Globals;
 
 import Video.VideoLibrary;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import kliveserver.CloudConnection;
 import uploaddownload.SendToFFMPeg;
 
@@ -17,7 +23,7 @@ import uploaddownload.SendToFFMPeg;
 public class GlobalData {
     public static String UserID = "USER"+System.currentTimeMillis();
     public static String RTPVideoStorePath = "C:\\Users\\home\\Videos\\KliveCache";
-    public static String ffplayPath = "C:\\Users\\home\\Documents\\ffmpeg\\bin\\ffplay.exe";
+    public static String ffplayPath = "Video\\ffplay.exe";
     public static String cloudIP = "";
     public static int cloudPort = 8080;
     public static String myIP = "";
@@ -34,6 +40,25 @@ public class GlobalData {
         RTPVideoStorePath = System.getProperty("java.io.tmpdir")+"\\KliveCache";
         videoLibrary = new VideoLibrary();
         sendToFFMPEG = new SendToFFMPeg();
+        try{
+            InputStream inputStream = videoLibrary.getClass().getResourceAsStream("/Video/ffplay.exe");
+            File ffplay = new File(RTPVideoStorePath+"\\ffplay.exe");
+            copyFile(inputStream, new FileOutputStream(ffplay));
+            ffplayPath = ffplay.getAbsolutePath();
+            inputStream.close();
+        } 
+        catch (IOException e) {
+          e.printStackTrace();
+        }
         sendToFFMPEG.start();
+    }
+    private static void copyFile(InputStream in, OutputStream out) throws IOException 
+    {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+          out.write(buffer, 0, read);
+        }
+        out.close();
     }
 }
